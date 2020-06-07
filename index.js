0df92b419c1385926dc832339d1a9e6e2cdbe01c
kulichokigor <igorkulichok@gmail.com>
2020-06-07 13:55:49 +0300

    third step, stylization

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const axios = require ('axios');
 
-import {KEY, apiUrl} from './safety/keys'
+import {KEY, apiUrl} from './safety/keys';
 
-const box = document.querySelector('#box');
+const city = document.querySelector('#city');
+const form = document.querySelector('form');
+const cityBox = document.querySelector('#name-city');
+const tempBox = document.querySelector('#temp');
+const weatherBox = document.querySelector('#weather-ico');
 
-const city = 'Khorol';
-
-const url = `${apiUrl}?q=${city}&appid=${KEY}&units=imperial`
-
-axios.get(url)
-    .then(resp=>{
-        const celsius = 5/9*(resp.data.main.temp - 32)
-        console.log(resp)
-        console.log(`${resp.data.name}: ${celsius.toFixed(0)}`)
-        box.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`
-        box.style.width = '50px'
-        box.style.height = '50px'
+    form.addEventListener('click',(event)=>{
+        event.preventDefault()
+        if(city !=null && city.value.length!=0){
+            const url = `${apiUrl}?q=${city.value}&appid=${KEY}&units=imperial`;
+            axios.get(url)
+            .then(resp=>{
+                const celsius = 5/9*(resp.data.main.temp - 32)
+                console.log(resp)
+                console.log(`${resp.data.name}: ${celsius.toFixed(0)}`)
+                cityBox.textContent = resp.data.name;
+                celsius.toFixed(0)>=0?tempBox.textContent = `+${celsius.toFixed(0)}`:tempBox.textContent = celsius.toFixed(0)
+    
+                cityBox.style.color = 'inherit';
+                tempBox.style.color = 'inherit';
+                weatherBox.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`
+                // box.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`
+            })
+            .catch(err=>{
+                cityBox.textContent = 'Problem';
+                cityBox.style.color = 'red';
+                tempBox.textContent = ':)'
+                tempBox.style.color = 'red';
+                weatherBox.style.background = 'inherit'
+                console.error(err.message)})
+        }
     })
-    .catch(err=>console.error(err))
