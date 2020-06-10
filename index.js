7e5cccdcd2c740dadc6eabb5f03761182b682d3d
kulichokigor <igorkulichok@gmail.com>
2020-06-10 19:47:42 +0300

    step three - change styles, output errors to the user

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const city = document.querySelector('#city');
 const form = document.querySelector('form');
 const cityBox = document.querySelector('#name-city');
 const tempBox = document.querySelector('#temp');
-const weatherBox = document.querySelector('#weather-ico');
+const weatherIcoBox = document.querySelector('#weather-ico');
+const weatherBox = document.querySelector('.app_temp-box');
+const startBox = document.querySelector('.app__start-box');
+const errorBox = document.querySelector('.error-box');
+const dataOutput = document.querySelector('#data-output__box')
 
     form.addEventListener('click',(event)=>{
         event.preventDefault()
@@ -14,6 +18,10 @@ const weatherBox = document.querySelector('#weather-ico');
             const url = `${apiUrl}?q=${city.value}&appid=${KEY}&units=imperial`;
             axios.get(url)
             .then(resp=>{
+                startBox.classList.add('remove')
+                weatherBox.classList.remove('remove')
+                errorBox.classList.add('remove')
+
                 const celsius = 5/9*(resp.data.main.temp - 32)
                 console.log(resp)
                 console.log(`${resp.data.name}: ${celsius.toFixed(0)}`)
@@ -22,15 +30,16 @@ const weatherBox = document.querySelector('#weather-ico');
     
                 cityBox.style.color = 'inherit';
                 tempBox.style.color = 'inherit';
-                weatherBox.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`
-                // box.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`
+                weatherIcoBox.style.background=`url( http://openweathermap.org/img/wn/${resp.data.weather[0].icon}.png)no-repeat`;
+
+
             })
             .catch(err=>{
-                cityBox.textContent = 'Problem';
-                cityBox.style.color = 'red';
-                tempBox.textContent = ':)'
-                tempBox.style.color = 'red';
-                weatherBox.style.background = 'inherit'
+                weatherBox.classList.add('remove');
+                startBox.classList.add('remove');
+                errorBox.classList.remove('remove');
+                dataOutput.style.background = '#5effc8';        // зміни стилів добавити через class!!!
+                dataOutput.style.border = '4px solid #0b4a3d'; // зміни стилів добавити через class!!!
                 console.error(err.message)})
         }
     })
